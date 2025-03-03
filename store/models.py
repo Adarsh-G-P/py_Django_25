879b7c4930065a338ef12b4d5a8cb24b8fdb5e77
Adarsh <adarshgp96@gmail.com>
2025-03-03 22:16:22 +0530

    one to many realtionship for class address

--- store/models.py
+++ store/models.py
@@ -43,4 +43,4 @@ class Order(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
-    customer = models.OneToOneField(Customer,on_delete=models.CASCACE, primary_key=True)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCACE)
